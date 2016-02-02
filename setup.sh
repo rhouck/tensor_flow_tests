ca3717cc682fe36566797cc832567ffab4770e62
ryan <ryan@ryan.com>
2016-02-02 13:25:19 -0500

    added mkdir to setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j 
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 docker build -t $USER/assignments .
+
+mkdir cache

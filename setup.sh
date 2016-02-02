fdaf773b8d0b6b36816ed15fc650c3deeea43135
ryan <ryan@ryan.com>
2016-02-02 10:22:36 -0500

    renamed iptables to setup and added docker build command

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ iptables -A INPUT -i eth0 -p tcp --dport 8888 -m state --state NEW,ESTABLISHED -
 iptables -A OUTPUT -o eth0 -p tcp --sport 8888 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+docker build -t $USER/assignments .
